5c4778593e47c68b2f33774ffb53c65216f7c44b
takusugiyama-hub <taku.sugiyama@gmail.com>
2026-08-29 20:19:41 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-
 const nextConfig: NextConfig = {
   output: "export",
-
-  basePath: isGithubActions ? "/manoii-site" : "",
-  assetPrefix: isGithubActions ? "/manoii-site/" : "",
 
   images: {
     unoptimized: true,

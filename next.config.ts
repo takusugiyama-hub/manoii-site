883070a1930630f356470281564dc52e2c45ec90
takusugiyama-hub <taku.sugiyama@gmail.com>
2026-08-29 20:10:17 +0900

    Update next.config.ts
    Fix GitHub Pages base path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: isGithubActions ? "/manoii-site" : "",
+  assetPrefix: isGithubActions ? "/manoii-site/" : "",
+
   images: {
     unoptimized: true,
   },
+
   allowedDevOrigins: [
     "192.168.86.120",
   ],
